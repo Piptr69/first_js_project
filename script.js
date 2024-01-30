3c7722bab2da673a1345e138ccad495ccad152b5
Piptr69 <p.paluchh02@gmail.com>
2024-01-30 16:33:18 +0100

    change loop creating divs

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 let gridContainer = document.querySelector('#grid');
-for (let i = 0; i < (16*16); i++)
+for (let i = 0; i < 16; i++)
 {
-    let div = createElement("div");
-    gridContainer.appendChild(div);
+    for (let j = 0; j < 16; j++)
+    {
+        let div = document.createElement("div");
+        div.textContent = "test";
+        gridContainer.appendChild(div);
+    }
+    
 }
