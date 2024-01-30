976c12ea21fda6703aed1fd94282c72c170d8ec2
Piptr69 <p.paluchh02@gmail.com>
2024-01-30 17:51:38 +0100

    resize divs

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ for (let i = 0; i < 16; i++)
     for (let j = 0; j < 16; j++)
     {
         let div = document.createElement("div");
-        div.textContent = "test";
+        div.classList.add("square");
         rows.appendChild(div);
     }
     gridContainer.append(rows);
