89edaa33d6270439f3cab9083435bc97d871bcbc
Piptr69 <p.paluchh02@gmail.com>
2024-01-30 16:42:29 +0100

    create 16x16 grid

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 let gridContainer = document.querySelector('#grid');
 for (let i = 0; i < 16; i++)
 {
+    let rows = document.createElement("div");
+    rows.classList.add("rows");
     for (let j = 0; j < 16; j++)
     {
         let div = document.createElement("div");
         div.textContent = "test";
-        gridContainer.appendChild(div);
+        rows.appendChild(div);
     }
-    
+    gridContainer.append(rows);
 }
